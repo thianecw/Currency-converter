ab4bb5c5eb4a639e72cf2821d0400b61f7e7a849
thianecw <thianecw@hotmail.com>
2023-12-05 20:19:06 +0000

    problema nas imagens

--- scripts.js
+++ scripts.js
@@ -2,33 +2,57 @@ const convertButton = document.querySelector(".convert-button")
 const curreciesSelect = document.querySelector(".currecies-select")
 
 function convertCurrency() {
-    const inputCurrency = document.querySelector(".input-currency")
+    const inputCurrency = document.querySelector(".input-currency").value
     const valueToBeConverted = document.querySelector(".value-To-Be-Converted")
     const valueConverted = document.querySelector(".value-converted")
 
     const usdToday = 4.96
     const eurToday = 5.34
 
-    if(curreciesSelect.value == "usd"){
+    if (curreciesSelect.value == "usd") {
         valueConverted.innerHTML = new Intl.NumberFormat("en-US", {
             style: "currency",
             currency: "USD"
         }).format(inputCurrency / usdToday)
     }
 
-    if(curreciesSelect.value == "eur"){
+    if (curreciesSelect.value == "eur") {
         valueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
             style: "currency",
             currency: "EUR"
         }).format(inputCurrency / eurToday)
+    }
 
+    valueToBeConverted.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrency)
 }
 
-valueToBeConverted.innerHTML = new Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "BRL"
-}).format(inputCurrency)
+function changeCurrency() {
+    const currencyName = document.getElementById("currency-name")
+    const currencyImage = document.querySelector("currency-img")
 
-convertButton.addEventListener("click", convertCurrency) 
+    if (curreciesSelect.value == "usd"){
+        currencyName.innerHTML = "American dolar"
+        currencyImage.src="./assets/usd.png"
+    }
+    if (curreciesSelect.value == "eur"){
+        currencyName.innerHTML = "Euro"
+        currencyImage.src="./assets/eur.png"
+     }
+    
+    // if (curreciesSelect.value == "pound"){
+    //     currencyName.innerHTML = "Pound sterling"
+    // }
+    // if (curreciesSelect.value == "btc"){
+    //     currencyName.innerHTML = "Biticoin"
+    // }
+    // if (curreciesSelect.value == "brl"){
+    //     currencyName.innerHTML = "Brazilian real"
+    // }
 
+    convertCurrency()
 }
+curreciesSelect.addEventListener("change", changeCurrency)
+convertButton.addEventListener("click", convertCurrency)
