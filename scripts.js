ca71f85d0de58375ae9e6d278892c0a21d724163
thianecw <thianecw@hotmail.com>
2023-12-05 20:57:43 +0000

    imagens funcionando

--- scripts.js
+++ scripts.js
@@ -8,6 +8,8 @@ function convertCurrency() {
 
     const usdToday = 4.96
     const eurToday = 5.34
+    const poundToday = 6.20
+    const btc = 215.205
 
     if (curreciesSelect.value == "usd") {
         valueConverted.innerHTML = new Intl.NumberFormat("en-US", {
@@ -23,6 +25,20 @@ function convertCurrency() {
         }).format(inputCurrency / eurToday)
     }
 
+    if (curreciesSelect.value == "pound") {
+        valueConverted.innerHTML = new Intl.NumberFormat("en-UK", {
+            style: "currency",
+            currency: "GBP"
+        }).format(inputCurrency / poundToday)
+    }
+
+    // if (curreciesSelect.value == "btc") {
+    //     valueConverted.innerHTML = new Intl.NumberFormat("en-UK", {
+    //         style: "currency",
+    //         currency: "GBP"
+    //     }).format(inputCurrency / poundToday)
+    // }
+
     valueToBeConverted.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL"
@@ -31,26 +47,30 @@ function convertCurrency() {
 
 function changeCurrency() {
     const currencyName = document.getElementById("currency-name")
-    const currencyImage = document.querySelector("currency-img")
+    const currencyImage = document.querySelector(".currency-image");
 
-    if (curreciesSelect.value == "usd"){
+    if (curreciesSelect.value == "usd") {
         currencyName.innerHTML = "American dolar"
-        currencyImage.src="./assets/usd.png"
+        currencyImage.src = "./assets/usd.png"
     }
-    if (curreciesSelect.value == "eur"){
+    if (curreciesSelect.value == "eur") {
         currencyName.innerHTML = "Euro"
-        currencyImage.src="./assets/eur.png"
-     }
-    
-    // if (curreciesSelect.value == "pound"){
-    //     currencyName.innerHTML = "Pound sterling"
-    // }
-    // if (curreciesSelect.value == "btc"){
-    //     currencyName.innerHTML = "Biticoin"
-    // }
-    // if (curreciesSelect.value == "brl"){
-    //     currencyName.innerHTML = "Brazilian real"
-    // }
+        currencyImage.src = "./assets/eur.png"
+    }
+
+    if (curreciesSelect.value == "pound") {
+        currencyName.innerHTML = "Pound sterling"
+        currencyImage.src = "./assets/pound.png"
+    }
+
+    if (curreciesSelect.value == "btc") {
+        currencyName.innerHTML = "Biticoin"
+        currencyImage.src = "./assets/btc.png"
+    }
+    if (curreciesSelect.value == "brl") {
+        currencyName.innerHTML = "Brazilian real"
+        currencyImage.src = "./assets/real.png"
+    }
 
     convertCurrency()
 }
